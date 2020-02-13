050d2b6a3e19b388f4aabebc66b1e649141a7322
Luis <luis901101@gmail.com>
2020-02-12 20:18:22 -0500

    gitignore updated

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,8 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/krrigan/Flutter/SDK"
 export "FLUTTER_APPLICATION_PATH=/Users/krrigan/Flutter/FlutterProjects/zsdk/example"
-export "FLUTTER_TARGET=/Users/krrigan/Flutter/FlutterProjects/zsdk/example/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/krrigan/Flutter/SDK/bin/cache/artifacts/engine/ios"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_FRAMEWORK_DIR=/Users/krrigan/Flutter/SDK/bin/cache/artifacts/engine/ios-release"
